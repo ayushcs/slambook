d02793ee9e535f5870a3d38a3273775dc02a1a14
ayushcs <ayushsrivastavacs@gmail.com>
2021-01-30 18:50:29 +0530

    commit

--- src/SlamBook.js
+++ src/SlamBook.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './App.css';
 import { Switch, Route, Link } from 'react-router-dom'
 import SlambookCreate from './component/SlambookCreate';
 import ViewSlamBook  from './component/ViewSlamBook';
 import Button from '@material-ui/core/Button';
-
+import logo from './image/main_logo.png'
 class SlamBook extends React.Component {
     constructor(props) {
         super(props);
@@ -26,12 +27,21 @@ class SlamBook extends React.Component {
                 {(showWelcomeButton)
                 ?
                 <div className="row">
-                    <Link to='/SlambookCreate'>
-                        <Button variant="outlined" onClick={()=>this.handleButton()} className="mb-2" out color="primary">Create SlamBook</Button>
-                    </Link>
-                    <Link to='/ViewSlamBook'>
-                        <Button variant="outlined" onClick={()=>this.handleButton()} className="mt-2" color="secondary">View SlamBook</Button>
-                    </Link>
+                    <div className= "col-12 px-0 mainimage">
+                    </div>
+                    <div className = "position-absolute col-12" style={{top: '40%'}}>
+                        <div className="text-center">
+                            <img src={logo} width="150px"/>
+                        </div>
+                        <div className="row mt-3">
+                            <Link className="col-12" to='/SlambookCreate'>
+                                <Button variant="contained" color="primary" onClick={()=>this.handleButton()} className="mb-2 col-12" >Create SlamBook</Button>
+                            </Link>
+                            <Link className="col-12" to='/ViewSlamBook'>
+                                <Button variant="contained" color="secondary" onClick={()=>this.handleButton()} className="mt-2 col-12">View SlamBook</Button>
+                            </Link>
+                        </div>
+                    </div>
                 </div>
                 :
                 null
